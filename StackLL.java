abafe80efa3da2ba2a5c804c7200dbbd6ceb4c02
Lawrence L <none@example.com>
2014-09-20 17:11:44 -0700

    Placed all DS in a single combined directory to keep compiling simple. Will figure how to package them later.

--- StackLL.java
+++ StackLL.java
@@ -13,17 +13,21 @@
 
 class StackLL{
     Node top = null;
+    private int N = 0;
 
     public void push(int item){
         Node newTop = new Node(item);
         newTop.next = top;
         top         = newTop;
+
+        ++N;
     }
 
     public Object pop(){
         if( !isEmpty() ){
             Object item = top.data;
             top      = top.next;
+            --N;
             return item;
         }
         //else
@@ -38,11 +42,23 @@ class StackLL{
         return top == null;
     }
 
+    public int size(){
+        return N;
+    }
+
 
     //Tester/Driver for StackLL.
     public static void main(String args[]){
         StackLL stk = new StackLL();
 
         assert stk.isEmpty();
+
+        //push odd numbers onto stack
+        for( int i=1; i < 10; i+=2 ){
+            stk.push(i);
+        }
+
+        assert stk.peek() == 9;
+
     }
 }
